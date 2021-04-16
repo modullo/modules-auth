d70cbb3d9336f97de636318d733c390bb0315521
tomideo1 <ayotomideaina@gmail.com>
2021-04-16 13:13:24 +0100

    dynamic forms modullo update

--- src/Http/Controllers/ModulesAuthController.php
+++ src/Http/Controllers/ModulesAuthController.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Exception;
 use Hostville\Modullo\Sdk;
 use Hostville\Modullo\UserLaravel\Auth\ModulloUser;
 use Hostville\Modullo\UserLaravel\Auth\ModulloUserProvider;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,13 +18,29 @@ use Illuminate\Support\Facades\Log;
 class ModulesAuthController extends \App\Http\Controllers\Controller
 {
   private $data;
+
   public function __construct() {
     $this->data = [
       'page' => ['title' => config('modules-auth.title')],
       'header' => ['title' => config('modules-auth.title')],
       'company_name' =>  config('modules-auth.view.company_name'),
-      'allow_registration' => config('modules-auth.view.allow_registration')
+      'allow_registration' => config('modules-auth.view.allow_registration'),
+      'custom_form_fields' => config('modules-auth.view.custom_form_fields'),
     ];
+
+  }
+
+
+  protected function loadUpdatableFields():array{
+    $customFields = [];
+    if ($this->data['custom_form_fields']){
+      $this->data['form_fields'] = config('modules-auth-form-fields.fields');
+      foreach ($this->data['form_fields'] as $field){
+        $customFields[$field['field_name']] = $field['field_name'];
+      }
+
+    }
+    return $customFields;
   }
 
   public function index(Request $request){
@@ -42,6 +59,7 @@ class ModulesAuthController extends \App\Http\Controllers\Controller
   public function showRegisterForm(){
 
     $this->data['company_logo'] = config('modules-auth.view.company_logo') ?? asset('vendor/modules-auth/logo/modullo.png');
+    $this->data['form_fields'] = config('modules-auth-form-fields.fields');
     return view('modules-auth::register',$this->data);
   }
 
@@ -89,9 +107,15 @@ class ModulesAuthController extends \App\Http\Controllers\Controller
   }
 
 
+  protected function addCustomFields(User $user, array $data): bool
+  {
+    $this->updateModelAttributes($user,$data,$this->loadUpdatableFields());
+    return $user->save();
+  }
+
+
 
   public function register(Request  $request, Sdk $sdk){
-
     $request->validate([
       'email' => 'required|email|unique:users',
       'password' => 'required',
@@ -99,6 +123,8 @@ class ModulesAuthController extends \App\Http\Controllers\Controller
       'first_name' => 'required',
       'phone_number' => 'required',
     ]);
+
+
      try {
        $response = create_account($sdk, [
          'email' => $request->input('email'),
@@ -107,6 +133,7 @@ class ModulesAuthController extends \App\Http\Controllers\Controller
          'last_name' => $request->input('last_name'),
          'phone_number' => $request->input('phone_number'),
        ]);
+
        if (!$response->isSuccessful()) {
          return redirect()->route('register')->withErrors(
            [
@@ -118,6 +145,7 @@ class ModulesAuthController extends \App\Http\Controllers\Controller
        }
        $user = null;
        $user = $this->create($user,$sdk,$response);
+       $this->addCustomFields($user,$request->all());
        if (!$user){
         return redirect()->route('register')->withErrors(['message' => 'account credentials could not be created']);
        }
@@ -147,6 +175,22 @@ class ModulesAuthController extends \App\Http\Controllers\Controller
     });
     return $user;
   }
+
+
+  protected function updateModelAttributes(Model $model, array $data, array $updateFields)
+  {
+    if (empty($updateFields)) {
+      throw new \UnexpectedValueException('The update fields were not configured for the endpoint.');
+    }
+    foreach ($updateFields as $requestKey => $modelKey) {
+      if (!array_key_exists($requestKey,$data)) {
+        # doesn't have it, so we skip the key
+        continue;
+      }
+      $model->{$modelKey} = $data[$requestKey];
+    }
+  }
+
 
 
 
---
+++ src/config/modules-auth-form-fields.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+  'fields' => [
+    [
+      'field_name' => 'profession',
+      'field_type' => 'enum',
+      'enum_data' => [
+        'developer',
+        'designer',
+        'project management'
+      ],
+    ],
+
+    [
+      'field_name' => 'address',
+      'field_type' => 'string',
+      'enum_data' => [],
+    ],
+
+  ]
+];
